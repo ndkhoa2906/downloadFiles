c412e2c6cd4532e2b631e696fe3c9e25c9f71d8c
Khoa Nguyen <nguyendangkhoa96nt@gmail.com>
2019-10-13 10:58:04 +0700

    Initial Commit

---
+++ MasterGCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasterGCD
+//
+//  Created by Monster on 10/13/19.
+//  Copyright © 2019 khoanguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
